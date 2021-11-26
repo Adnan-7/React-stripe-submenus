c2568662b610a94fb3f9c20729b6b51dfe1ef64a
Adnan Khan <adnan7.zain@gmail.com>
2021-11-26 22:05:45 +0500

    display the navbar

--- src/navebar.js
+++ src/navebar.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import logo from './images/logo.svg';
+import { useGlobalContext } from './context';
 
 const Navebar = () => {
-  return <div>navbar comp</div>;
+  const { openSidbar, openSubmenu, closeSubmenu } = useGlobalContext();
+  return (
+    <nav className='nav'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='stripe' />
+          <button className='btn toggle-btn' onClick={openSidbar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'>
+          <li>
+            <button className='link-btn'>products</button>
+          </li>
+          <li>
+            <button className='link-btn'>developers</button>
+          </li>
+          <li>
+            <button className='link-btn'>company</button>
+          </li>
+        </ul>
+
+        <button className='btn signin-btn'>sign in</button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navebar;
